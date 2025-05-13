72cbdaaabe752a41dab381046924ca1729e0d57f
Willian Alves Moreno <willianam@uninove.br>
2025-05-13 18:57:01 -0300

    feat: Customizando o tema padrão do gluestack

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar, View } from 'react-native'
 
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
-import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+
+import { config } from './config/gluestack-ui.config'
 
 export function App() {
   const [fontsLoaded] = useFonts({
@@ -10,21 +12,17 @@ export function App() {
   })
 
   return (
-    <GluestackUIProvider>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#202024',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {fontsLoaded ? (
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Center
+          flex={1}
+          bg="$gray700"
+        >
           <Text
             fontSize={24}
             fontFamily="Roboto_700Bold"
@@ -32,10 +30,10 @@ export function App() {
           >
             Home
           </Text>
-        ) : (
-          <View />
-        )}
-      </View>
+        </Center>
+      ) : (
+        <View />
+      )}
     </GluestackUIProvider>
   )
 }
