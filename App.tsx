9be81e5674f179060ae0e7f2e058132b3eaaeac6
Willian Alves Moreno <willianam@uninove.br>
2025-05-13 18:42:29 -0300

    chore: Instalando o Gluestack

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
 
 export function App() {
   const [fontsLoaded] = useFonts({
@@ -9,24 +10,32 @@ export function App() {
   })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#202024',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: 'Roboto_700Bold', color: '#fff' }}>Home</Text>
-      ) : (
-        <View />
-      )}
-    </View>
+    <GluestackUIProvider>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#202024',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? (
+          <Text
+            fontSize={24}
+            fontFamily="Roboto_700Bold"
+            color="white"
+          >
+            Home
+          </Text>
+        ) : (
+          <View />
+        )}
+      </View>
+    </GluestackUIProvider>
   )
 }
