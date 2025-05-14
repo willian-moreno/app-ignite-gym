9b2469e5303288f4d6510bd82e96ec1b204734c3
Willian Alves Moreno <willianam@uninove.br>
2025-05-14 09:55:29 -0300

    feat: Finalizando estilização e configuração inputs

---
+++ src/components/input.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from 'react'
+
+import { Input as GluestackInput, InputField } from '@gluestack-ui/themed'
+
+type Props = {} & ComponentProps<typeof InputField>
+
+export function Input({ ...props }: Props) {
+  return (
+    <GluestackInput
+      bg="$gray700"
+      h="$14"
+      px="$4"
+      borderWidth="$0"
+      borderRadius="$md"
+      $focus={{
+        borderWidth: 1,
+        borderColor: '$green500',
+      }}
+    >
+      <InputField
+        color="$white"
+        fontFamily="$body"
+        placeholderTextColor="$gray300"
+        {...props}
+      />
+    </GluestackInput>
+  )
+}
--- src/screens/sign-in.tsx
+++ src/screens/sign-in.tsx
@@ -1,7 +1,9 @@
-import { Center, Image, Text, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
+
+import { Input } from '@components/input'
 
 export function SignIn() {
   return (
@@ -17,15 +19,35 @@ export function SignIn() {
         h="$3/4"
         position="absolute"
       />
-      <Center my="$24">
-        <LogoSvg />
-        <Text
-          color="$gray100"
-          fontSize="$sm"
-        >
-          Treine sua mente e o seu corpo.
-        </Text>
-      </Center>
+      <VStack
+        flex={1}
+        px="$10"
+        pb="$16"
+      >
+        <Center my="$24">
+          <LogoSvg />
+          <Text
+            color="$gray100"
+            fontSize="$sm"
+          >
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color="$gray100">Acesse a conta</Heading>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input
+            placeholder="Senha"
+            autoCapitalize="none"
+            secureTextEntry
+          />
+        </Center>
+      </VStack>
     </VStack>
   )
 }
