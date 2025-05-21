078114c1c849427d4926e5f5de091c4eec6d632f
Willian Alves Moreno <willianam@uninove.br>
2025-05-21 18:30:49 -0300

    feat: Criando lista de grupos

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { VStack } from '@gluestack-ui/themed'
 
+import { Group } from '@components/group'
 import { HomeHeader } from '@components/home-header'
 
+import { gluestackUIConfig } from './../../config/gluestack-ui.config'
+
 export function Home() {
+  const { tokens } = gluestackUIConfig
+
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 32,
+          columnGap: tokens.space['3'],
+        }}
+        style={{
+          marginVertical: 40,
+          maxHeight: 44,
+          minHeight: 44,
+        }}
+        horizontal
+      />
     </VStack>
   )
 }
